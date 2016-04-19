e10007380ae36d4b28f58dd3abf9aa7e9250eea5
Artur Pañach <arturictus@gmail.com>
2016-04-19 20:35:22 +0200

    added test for address city

---
+++ test/address_test.exs
@@ -0,0 +1,21 @@
+defmodule AddressTest do
+  use ExUnit.Case
+
+  test "#city" do
+    first = Faker.Address.city
+    assert is_binary(first)
+    assert String.length(first) > 0
+    second = Faker.Address.city
+    assert is_binary(second)
+    assert String.length(second) > 0
+    assert first != second
+  end
+
+  test "random generation" do
+    ary = []
+    for _ <- 0..10 do
+      ary = [ Faker.Address.city | ary ]
+    end
+    assert Enum.count(List.uniq(ary)) > 1
+  end
+end
